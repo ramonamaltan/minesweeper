a6f8df45655e92cdc3e3749654afa7d6ce2fb764
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-05 10:31:24 +0100

    add field number logic

--- app.js
+++ app.js
@@ -22,13 +22,42 @@ document.addEventListener('DOMContentLoaded', () => {
       const square = document.createElement('div')
       // add numbered id to them
       square.setAttribute('id', i)
-      // add 'bomb' or 'valid' to each field
+      // add 'bomb' or 'valid' class to each field
       square.classList.add(shuffledArray[i])
       // place divs inside grid
       grid.appendChild(square)
       // push all squares into above defined squares array
       squares.push(square)
     }
+
+    // add numbers (numbers show up on fields when there's bombs around it - represents number of bombs around that field)
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0
+      // edge cases would count in squares on the other side as 'around them' - we don't want that
+      // define left edge e.g. number 10 if it's remainder of division through width (10) = 0 then it's on the left
+      const isLeftEdge = (i % width === 0)
+      const isRightEdge = (i % width === width - 1)
+
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
+        if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
+        if (i > 10 && squares[i -width].classList.contains('bomb')) total++
+        if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total++
+        if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total++
+        if (i < 99 && !isLeftEdge && squares[i -1 +width].classList.contains('bombs')) total++
+        if (i < 88 && !isRightEdge && squares[i +1 +width].classList.contains('bombs')) total++
+        if (i < 89 && squares[i +width].classList.contains('bombs')) total++
+        squares[i].setAttribute('data', total)
+        console.log(squares[i])
+      }
+    }
+
+
+
+
+
+
+
   }
   createBoard()
 
